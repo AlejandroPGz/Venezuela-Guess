6b39febfd6a30977b0b396a505a5f98ff1b580bb
Alejandro <alejandroparadiso02@gmail.com>
2024-07-31 19:10:11 -0400

    auth

--- app/auth/sing-in/index.js
+++ app/auth/sing-in/index.js
@@ -1,34 +1,77 @@
-import { View, Text, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, ToastAndroid } from 'react-native'
+import React, { useState } from 'react'
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useRouter } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { auth } from "../../../configs/FirebaseConfig";
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function SingIn() {
 
   const insets = useSafeAreaInsets();
+  const router = useRouter();
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const onSingIn = () => {
+
+    if (!email&&!password) {
+      ToastAndroid.show("Please", ToastAndroid.LONG)
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage, errorCode);
+    if (errorCode == "auth/invalid-credential") {
+      ToastAndroid.show("Invalid", ToastAndroid.LONG)
+    }
+  });
+
+  }
 
   return (
-    <View className="w-screen h-screen bg-scarpa-flow-50 px-4 justify-center" style={{  paddingTop: insets.top, paddingBottom: insets.bottom }}>
-      <Text className="text-4xl font-semibold text-scarpa-flow-950">Bienvenido de vuelta</Text>
+    <View className="w-screen h-screen bg-scarpa-flow-50 px-4 mt-2" style={{  paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <TouchableOpacity>
+        <Ionicons 
+        onPress={() => router.push("/")}
+        style={{marginTop: 10}} name="chevron-back" size={24} color="black" />
+      </TouchableOpacity>
+      <Text className="text-4xl font-semibold text-scarpa-flow-950 mt-8">Bienvenido de vuelta</Text>
       <Text className="text-3xl font-medium mt-6 text-scarpa-flow-500">Te extrañamos...</Text>
       <Text className="text-3xl font-medium mt-6 text-scarpa-flow-500">Inicia Sesión</Text>
       <View className="mt-10">
         <Text className="text-xl font-medium">Email</Text>
-        <TextInput placeholder='YourEmail@example.com' className="border-input rounded-lg py-3 px-4 shadow-xl shadow-black bg-white mt-2"></TextInput>
+        <TextInput 
+        onChangeText={(value)=> {setEmail(value)}}
+        placeholder='YourEmail@example.com' className="border-input rounded-lg py-3 px-4 shadow-xl shadow-black bg-white mt-2"></TextInput>
       </View>
       <View className="mt-4">
         <Text className="text-xl font-medium">Password</Text>
         <TextInput 
+        onChangeText={(value)=> {setPassword(value)}}
         secureTextEntry={true}
         placeholder='' 
         className="border-input rounded-lg py-3 px-4 shadow-xl shadow-black bg-white mt-2"></TextInput>
       </View>
 
-      <View className="w-full bg-scarpa-flow-950 h-16 mt-10 rounded-xl justify-center items-center"> 
+      <TouchableOpacity 
+      onPress={onSingIn}
+      className="w-full bg-scarpa-flow-950 h-16 mt-10 rounded-xl justify-center items-center"> 
         <Text className="text-lg text-scarpa-flow-50 font-extrabold">Sing In</Text>
-      </View>
-      <View className="w-full bg-scarpa-flow-50 h-16 mt-10 rounded-xl justify-center items-center border-input"> 
+      </TouchableOpacity>
+      <TouchableOpacity 
+      onPress={() => router.push("auth/sing-up")}
+      className="w-full bg-scarpa-flow-50 h-16 mt-10 rounded-xl justify-center items-center border-input"> 
         <Text className="text-lg text-scarpa-flow-950 font-extrabold ">Create Account</Text>
-      </View>
+      </TouchableOpacity>
     </View>
   )
 }
--- app/auth/sing-up/index.js
+++ app/auth/sing-up/index.js
@@ -1,15 +1,88 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, ToastAndroid } from 'react-native'
+import React, { useState } from 'react'
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useRouter } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../configs/FirebaseConfig";
 
 export default function SingUp() {
 
   const insets = useSafeAreaInsets();
+  const router = useRouter();
+
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [user, setUser] = useState()
+
+  const onCreateAccount = () => {
+
+    if (!email&&!password&&!user) {
+      ToastAndroid.show("Please", ToastAndroid.LONG)
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+    // ..
+  });
+  }
 
 
   return (
-    <View className="w-screen h-screen bg-scarpa-flow-100" style={{  paddingTop: insets.top, paddingBottom: insets.bottom }}>
-      <Text>Sing up</Text>
+    
+    <View className="w-screen h-screen bg-scarpa-flow-50 px-4" style={{  paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <TouchableOpacity>
+        <Ionicons 
+        onPress={() => router.push("auth/sing-in")}
+        style={{marginTop: 10}} name="chevron-back" size={24} color="black" />
+      </TouchableOpacity>
+    <Text className="text-4xl font-semibold text-scarpa-flow-950 mt-8">Crear Cuenta</Text>
+    <View className="mt-10">
+      <Text className="text-xl font-medium">Email</Text>
+      <TextInput 
+      onChangeText={(value)=> {setEmail(value)}}
+      placeholder='YourEmail@example.com' className="border-input rounded-lg py-3 px-4 shadow-xl shadow-black bg-white mt-2"></TextInput>
     </View>
+    <View className="mt-4">
+      <Text className="text-xl font-medium">Usuario</Text>
+      <TextInput 
+      onChangeText={(value)=> {setUser(value)}}
+      placeholder='usuario' className="border-input rounded-lg py-3 px-4 shadow-xl shadow-black bg-white mt-2"></TextInput>
+    </View>
+    <View className="mt-4">
+      <Text className="text-xl font-medium">Password</Text>
+      <TextInput 
+      onChangeText={(value)=> {setPassword(value)}}
+      secureTextEntry={true}
+      placeholder='' 
+      className="border-input rounded-lg py-3 px-4 shadow-xl shadow-black bg-white mt-2"></TextInput>
+    </View>
+    {/* <View className="mt-4">
+      <Text className="text-xl font-medium">Confirm Password</Text>
+      <TextInput 
+      secureTextEntry={true}
+      placeholder='' 
+      className="border-input rounded-lg py-3 px-4 shadow-xl shadow-black bg-white mt-2"></TextInput>
+    </View> */}
+    <TouchableOpacity 
+    onPress={onCreateAccount}
+    className="w-full bg-scarpa-flow-950 h-16 mt-10 rounded-xl justify-center items-center"> 
+      <Text className="text-lg text-scarpa-flow-50 font-extrabold">Crear</Text>
+    </TouchableOpacity>
+    <TouchableOpacity 
+    onPress={() => router.push("auth/sing-in")}
+    className="mt-10 items-center w-min">
+      <Text className="text-lg font-semibold">Already have an account? Sing in</Text>
+    </TouchableOpacity>
+  </View>
   )
 }
